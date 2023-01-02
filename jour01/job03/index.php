d6ff626ec99aed233cde8a1ff728cd4abd5e0860
miguel-molia <miguel.molia@laplateforme.io>
2023-01-03 00:24:43 +0100

    Job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,4 +1,4 @@
-<?php ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,15 +9,48 @@
 </head>
 
 <body>
+
+<?php 
+
+    $myname = "Miguel";
+    $myage = "31";
+    $myrecord = "18.4";
+    $isCorrect = "true";
+
+
+?>
+
 <table>
-    <thead>
+    <caption>Variables</caption>
+    
         <tr>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
         </tr>
-    </thead>
+        <tr>
+            <td>Boolean</td>
+            <td>Bool</td>
+            <td>True</td>
+        </tr>
+        <tr>
+            <td>Entier</td>
+            <td>Int</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <td>Chaines de caracteres</td>
+            <td>String</td>
+            <td>Je suis en formation</td>
+        </tr>
+        <tr>
+            <td>Nombre a virgule flottante</td>
+            <td>Float</td>
+            <td>4.21</td>
+        </tr>
+    
 </table>
+
 
 
 
