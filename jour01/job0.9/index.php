fd02526a8cf9f5daf13e360f41d8fcb0f89d8c28
miguel-molia <miguel.molia@laplateforme.io>
2023-01-02 11:19:33 +0100

    initial commit

---
+++ jour01/job0.9/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Le titre de ma page</title>
+    </head>
+    <body>
+
+    <?php echo "Hello LaPlateforme!";
+    ?>
+    </body>
+</html> 
