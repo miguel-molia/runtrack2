4eaf21ba315ff42ff44d4c23fde66d69d21dba39
miguel-molia <miguel.molia@laplateforme.io>
2023-01-04 16:55:02 +0100

    job 3

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -2,19 +2,21 @@
 
 for ($x=0; $x<=100; $x=$x+1)
 {
-    echo $x . "</br>";
     
     if ($x>=0 && $x<=20) {
         echo "<i>".$x."</i>"."</br>";
     }
-
-    if ($x>=25 && $x<=50) {
-        echo "<u>".$x."</u>"."</br>";
-    }
-
-    if ($x==42) {
+    else if ($x==42) {
         echo "LaPlateforme"."</br>";
     }
+    else if ($x>=25 && $x<=50) {
+        echo "<u>".$x."</u>"."</br>";
+    }
+    
+   
+    
+    else { echo $x . "</br>";}
+     
 }
 
 
