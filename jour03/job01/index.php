508fcf561110c3b6b5aebe3d648286330241dbee
miguel-molia <miguel.molia@laplateforme.io>
2023-01-04 13:43:54 +0100

    job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$nombre= [
+     200,204,98,404,
+    173,171,459,
+  
+];
+
+foreach ($nombre as $value)
+{
+    if ($value % 2 == 0)
+    {
+        echo $value . " " ."est pair" . "</br>";
+    }
+
+    else {
+        echo $value . " " ."est impair" . "</br>";
+    }
+}
+
+
+
+
+
+
+
+
+?>
