750f1ccf454b0d823684cfeadc9713f21d0a91d5
miguel-molia <miguel.molia@laplateforme.io>
2023-01-11 13:39:43 +0100

    Runtrack2 modified

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -5,16 +5,16 @@ $str = "I'm sorry Dave I'm afraid I can't do that.";
 $voy= "aeiouyAEIOUY";
 
 for ($x=0; isset ($str[$x]) == true; $x=$x+1)
-{
+{//pour la variable str
     
     
     for ($y=0; isset ($voy[$y]) == true; $y=$y+1)
-    {  
+    {  //pour la variable voy dans la variable str (connecter les deux)
 
         if ($str[$x] == $voy[$y])
-        {
+        {//si la variable str de $x est indentique à la variable voy de $y
             echo $str[$x];
-        }
+        }// tu m'affiches la variable str de $x
 
 
     }
--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -4,7 +4,8 @@ $str = "Dans l'espace, personne ne vous entend crier.";
 
 for ($x=0; isset($str[$x])==true; $x++)
 {
-    echo $x;
+    
 }
+echo $x;
 
 ?>
